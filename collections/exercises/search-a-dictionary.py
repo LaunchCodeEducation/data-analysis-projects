bc7f69c8133a2caef085e135d3659cafd436e777
DataLore777 <willmanap@gmail.com>
2024-08-27 14:57:59 -0500

    Homework complete

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -1,32 +1,31 @@
-# Write your return_cost function here:
-def return_cost(menu, item):
-  pass
+flavors = {
+  'chocolate' : 0.35,
+  'vanilla' : 0.35,
+  'strawberry' : 0.42,
+  'cookies and cream' : 0.45,
+  'mint chocolate chip' : 0.42,
+  'fudge chunk' : 0.45,
+  'saffron' : 2.25,
+  'garlic' : 0.05
+}
 
-# Write your fanciest_flavor function here:
+choice = 'cookies and cream'
 
+if choice in flavors:
+    cost = flavors[choice]
+else: 
+    cost = 0
 
-def main():
-  flavors = {
-    'chocolate' : 0.35,
-    'vanilla' : 0.35,
-    'strawberry' : 0.42,
-    'cookies and cream' : 0.45,
-    'mint chocolate chip' : 0.42,
-    'fudge chunk' : 0.45,
-    'saffron' : 2.25,
-    'garlic' : 0.05
-  }
+print("The cost of", choice, "is", cost)
 
-  choice = 'vanilla'
-  price = return_cost(flavors, choice)
-  if price == 0:
-    print("Sorry, we don't have {0}.".format(choice))
-  else:
-    print(f"The price for {choice} is ${price} per scoop.")
+# Part 2
 
-# Uncomment the lines below after you code your fanciest_flavor function.
-  # print('---')
-  # expensive_flavor = fanciest_flavor(flavors)
-  # print(f"The most expensive flavor we have is {expensive_flavor}.")
+highest_cost = 0
+fanciest = ""
 
-main()
+for flavor in flavors:
+    if flavors[flavor] > highest_cost:
+        fanciest = flavor
+        highest_cost = flavors[flavor]
+
+print("The fanciest flavor is", fanciest, "and it costs", highest_cost)
