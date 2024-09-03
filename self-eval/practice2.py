da7c838b6f338fd82340cc8535da1e31ac832668
DataLore777 <willmanap@gmail.com>
2024-09-03 18:23:06 -0500

    Learn how def indent changes returns

--- self-eval/practice2.py
+++ self-eval/practice2.py
@@ -1,3 +1,4 @@
+# practice shadow variables or parameters or whatever it's called
 def remove_hyphens(phone_number):
    without_hyphens = phone_number.replace('-', '')
    return without_hyphens
@@ -5,4 +6,36 @@ def remove_hyphens(phone_number):
 phone_number = "614-555-5555"
 no_hyph_number = remove_hyphens('56-78')
 
-print (remove_hyphens(phone_number))
+# print (remove_hyphens(phone_number))
+
+
+
+# num less than 100 rather than total less than 100
+def add_numbers_together(num):
+   total = 0
+   while num < 100:
+      total += num
+      num += 1
+   return total
+
+print(add_numbers_together(1))
+   
+# total less than 100. print() final total part of function
+def add_numbers_together(num):
+   total = 0
+   while total < 100:
+      total += num
+      num += 1
+   print(total)
+
+add_numbers_together(1)
+
+# total less than 100. print() ongoing total part of function.
+def add_numbers_together(num):
+   total = 0
+   while total < 100:
+      total += num
+      num += 1
+      print(total)
+
+add_numbers_together(1)
